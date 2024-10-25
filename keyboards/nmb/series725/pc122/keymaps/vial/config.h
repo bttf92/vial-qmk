2cc19a44e1ded9dc4060b7ebea7963cfcffd20bd
Luca Di Giovanni <lucadigiovanni@outlook.it>
2024-10-25 22:20:17 +0200

    Added NMB PC122 keyboard

---
+++ keyboards/nmb/series725/pc122/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x05, 0x1F, 0x57, 0x39, 0x85, 0x7E, 0xEA, 0x6A}
+#define VIAL_UNLOCK_COMBO_ROWS { 7, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
